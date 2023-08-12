a06f582301534c3c5093dc6b10566d47749b5738
honk <github@honk.email>
2023-08-12 15:30:08 +0200

    fix: update lang to v3

--- src/main/java/com/honklol/guardian/config/yaml/CommentedConfiguration.java
+++ src/main/java/com/honklol/guardian/config/yaml/CommentedConfiguration.java
@@ -28,7 +28,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 
-import org.apache.commons.lang.Validate;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.InvalidConfigurationException;
